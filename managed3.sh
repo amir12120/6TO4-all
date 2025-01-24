e71a1fdf26bd1510a2dc8d4b7a127290a50810aa
Kalilovers <30160766+kalilovers@users.noreply.github.com>
2025-01-24 17:58:33 +0330

    Update managed3.sh

--- managed3.sh
+++ managed3.sh
@@ -19,4 +19,4 @@ else
     echo "Package python3-colorama not found in the repositories."
 fi
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Azumi67/6TO4-GRE-IPIP-SIT/main/ubuntu24.sh)"
+bash -c "$(curl -fsSL https://github.com/kalilovers/6TO4-all/blob/main/ubuntu24.sh)"
